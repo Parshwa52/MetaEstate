68964a3d1b12512d7c45f8901cfb2c7699f4c9e5
Parshwa52 <parshwashahsbmp52@gmail.com>
2023-01-05 10:49:45 +0530

    Added browserrouter in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -15,12 +15,12 @@ const darkTheme = createTheme({
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-
+      <BrowserRouter>
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
         <App />
       </ThemeProvider>
-
+      </BrowserRouter>
   </React.StrictMode>
 );
 
